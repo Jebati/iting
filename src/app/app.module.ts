ed97671a57f7f1d72d1d015b6e91b5ed6fe7fa4d
Jebati <www.den-safonov@mail.ru>
2021-01-26 19:43:46 +0300

    create category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,20 @@ import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+
 import { GoodsComponent } from './components/goods/goods.component';
 import { GoodCreateComponent } from './components/good-create/good-create.component';
 import { GoodEditComponent } from './components/good-edit/good-edit.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { CategoryCreateComponent } from './components/category-create/category-create.component';
 import { CategoryEditComponent } from './components/category-edit/category-edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -36,8 +42,14 @@ import { CategoryEditComponent } from './components/category-edit/category-edit.
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
     AngularFirestoreModule,
+    MatInputModule,
     MatToolbarModule,
     MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/category-create/category-create.component.ts
+++ src/app/components/category-create/category-create.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/entities/categories.entity';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+  FormBuilder,
+} from '@angular/forms';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-category-create',
   templateUrl: './category-create.component.html',
-  styleUrls: ['./category-create.component.scss']
+  styleUrls: ['./category-create.component.scss'],
 })
 export class CategoryCreateComponent implements OnInit {
+  public category: Category;
+  public form: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    fields: this.fb.array([]),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public get fields(): FormArray {
+    return <FormArray>this.form.controls.fields;
   }
 
+  constructor(private fb: FormBuilder, private db: AngularFireDatabase) {}
+
+  ngOnInit(): void {}
+
+  addField() {
+    this.fields.push(
+      this.fb.group({
+        key: ['', [Validators.required]],
+        type: ['0'],
+      })
+    );
+  }
+
+  removeField(i: number) {
+    this.fields.removeAt(i);
+  }
+
+  addCategory() {
+    this.db
+      .list('categories', (ref) =>
+        ref.orderByChild('name').equalTo(this.form.value.name)
+      )
+      .snapshotChanges()
+      .subscribe((categories) => {
+        if (categories.length) {
+          if(confirm("Данная категория уже существует. Применить изменения к ней?")) {
+            this.db.list('categories').set(categories[0].key, this.form.value);
+          }
+        } else this.db.list('categories').push(this.form.value);
+      });
+  }
 }
